5b8a109b68fa0e8870b45eb06b09b04e626df089
merkanb <merkanb@gmail.com>
2023-07-05 21:33:01 +0200

    ErkanClass uploaded

--- src/Erkan.java
+++ src/Erkan.java
@@ -1,2 +1,5 @@
 public class Erkan {
+    public static void main(String[] args) {
+        System.out.println("hi..");
+    }
 }
---
+++ src/Room1/ErkanClass.java
@@ -0,0 +1,7 @@
+package Room1;
+
+public class ErkanClass {
+    public static void main(String[] args) {
+        System.out.println("hi this is my test class");
+    }
+}

65ae921f03bb3135fc41e204c0a394fe08ac3c1c
Talha <talha43gs@gmail.com>
2023-07-05 19:56:53 +0100

    Project setup

---
+++ src/GeneralTest.java
@@ -0,0 +1,18 @@
+public class GeneralTest {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void test1(){
+        System.out.println("TEST1");
+    }
+
+    public void test2(){
+        System.out.println("TEST2");
+    }
+
+    public void test3(){
+        System.out.println("TEST3");
+    }
+}
---
+++ src/Room3/Room3.java
@@ -0,0 +1,4 @@
+package Room3;
+
+public class Room3 {
+}

9c28d4cb8ab5c2b063c02900a62f171ef6e34e80
Talha <talha43gs@gmail.com>
2023-07-06 18:19:50 +0100

    Updated main method

--- src/Ashwini.java
+++ src/Ashwini.java
@@ -1,3 +1,9 @@
 public class Ashwini {
 
+    public static void main(String[] args) {
+        System.out.println(3/1);
+        System.out.println("test!!!!");
+        System.out.println("BLABALBALBALALBBALLBLA");
+    }
+
 }

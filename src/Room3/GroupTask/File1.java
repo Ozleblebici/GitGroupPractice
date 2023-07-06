2a1333d07e95ee52f45a1a52cb5d58b52d996089
AshwiniD <ashwini.dhaneppagol@gmail.com>
2023-07-06 12:24:07 +0200

    Changed 7 to 1 in file1

--- src/Room3/GroupTask/File1.java
+++ src/Room3/GroupTask/File1.java
@@ -2,7 +2,7 @@ package Room3.GroupTask;
 
 public class File1 {
     public static void main(String[] args) {
-        int a=7;
+        int a=1;
         int b=4;
         System.out.println(a*b);
     }

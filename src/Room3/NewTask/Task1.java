8faaf0d4578d55e2fade3d66ff3a4e9df16ecea0
Talha <talha43gs@gmail.com>
2023-07-06 17:38:14 +0100

    Updated Ashwini's method

--- src/Room3/NewTask/Task1.java
+++ src/Room3/NewTask/Task1.java
@@ -2,6 +2,8 @@ package Room3.NewTask;
 
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("Hi ");
+
+        System.out.println("Hi updated code ");
+        System.out.println("SDASDSADASDASDASDA");
     }
 }

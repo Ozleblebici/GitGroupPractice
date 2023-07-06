2819bf1c885cbed66b22ea11f095699bf1a8b28b
AshwiniD <ashwini.dhaneppagol@gmail.com>
2023-07-06 19:23:55 +0200

    add new test

--- src/Ashwini.java
+++ src/Ashwini.java
@@ -1,3 +1,13 @@
 public class Ashwini {
 
+    public static void test(){
+        System.out.println("TESTTTTTTT");
+    }
+
+    public static void main(String[] args) {
+        test();
+        System.out.println("Test done!");
+    }
+
+
 }

5d3d1aa55da359e482218a1e7e487bf00834f41a
merkanb <merkanb@gmail.com>
2023-07-07 13:08:58 +0200

    ErkanNonConflictNewClass added and G3 updated

--- src/ConflictHomework/G3.java
+++ src/ConflictHomework/G3.java
@@ -7,5 +7,6 @@ public class G3 {
         System.out.println("TEST1 updated  by Erkan... ");
         System.out.println("TEST2");
         System.out.println("TEST3");
+        System.out.println("Erkan conflict ");
     }
 }

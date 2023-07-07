7b497bcabf1a98f3de8757ccd962e94e1cf175f7
User <seyfettinhasoz@hotmail.com>
2023-07-07 12:41:00 +0200

    Changes

--- src/ConflictHomework/G4.java
+++ src/ConflictHomework/G4.java
@@ -3,11 +3,7 @@ package ConflictHomework;
 public class G4 {
     public static void main(String[] args) {
         System.out.println("TEST1");
-        int c= 20;
-        int d=30;
-        System.out.println(c+d);
-
-        System.out.println("TEST2");
+        System.out.println("this line updated by Seyfettin");
         System.out.println("TEST3");
     }
 }

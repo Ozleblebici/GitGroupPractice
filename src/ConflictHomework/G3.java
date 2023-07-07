9fb6064fd3bb125dad804071c6077cafa2957941
meral-das <137941954+meral-das@users.noreply.github.com>
2023-07-07 11:22:24 +0100

    meral uploaded uploaded

--- src/ConflictHomework/G3.java
+++ src/ConflictHomework/G3.java
@@ -2,7 +2,7 @@ package ConflictHomework;
 
 public class G3 {
     public static void main(String[] args) {
-        System.out.println("TEST1");
+        System.out.println("Meral");
         System.out.println("TEST2");
         System.out.println("TEST3");
     }

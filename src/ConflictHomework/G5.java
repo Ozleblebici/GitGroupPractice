d673a83c454fae707d3b128a01e7df6d849f2552
cihat <cihatalici04@gmail.com>
2023-07-07 08:58:02 +0100

    Conflict Files Have Uploaded

--- src/ConflictHomework/G5.java
+++ src/ConflictHomework/G5.java
@@ -2,7 +2,7 @@ package ConflictHomework;
 
 public class G5 {
     public static void main(String[] args) {
-        System.out.println("TEST1");
+        System.out.println("Check1");
         System.out.println("TEST2");
         System.out.println("TEST3");
     }

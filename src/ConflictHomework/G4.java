c3006f550212b51b3e7535fc560d1b384cb40b46
Monika <monikasturman@gmail.com>
2023-07-07 11:40:47 +0100

    we changed some bit

--- src/ConflictHomework/G4.java
+++ src/ConflictHomework/G4.java
@@ -3,7 +3,7 @@ package ConflictHomework;
 public class G4 {
     public static void main(String[] args) {
         System.out.println("TEST1");
-        System.out.println("TEST2");
+        System.out.println("This line updated by Monic");
         System.out.println("TEST3");
     }
 }
